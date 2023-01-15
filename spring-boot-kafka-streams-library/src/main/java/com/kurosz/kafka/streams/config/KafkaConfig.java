90b9564855bc40b15ad044b74ae0dee8b121c619
Daniel Kurosz <danielkurosz@gmail.com>
2023-01-15 16:20:19 +0100

    update build files to support modules building, define kafka and spring versions

--- spring-boot-kafka-streams-library/src/main/java/com/kurosz/kafka/streams/config/KafkaConfig.java
+++ spring-boot-kafka-streams-library/src/main/java/com/kurosz/kafka/streams/config/KafkaConfig.java
@@ -1,2 +1,37 @@
-package com.kurosz.kafka.streams.config;public class KafkaConfig {
+package com.kurosz.kafka.streams.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.annotation.EnableKafkaStreams;
+import org.springframework.kafka.annotation.KafkaStreamsDefaultConfiguration;
+import org.springframework.kafka.config.KafkaStreamsConfiguration;
+
+import java.util.HashMap;
+
+@Configuration
+@EnableKafka
+@EnableKafkaStreams
+public class KafkaConfig {
+
+    private String bootstrapServer;
+
+    public KafkaConfig(String bootstrapServer) {
+        this.bootstrapServer = bootstrapServer;
+    }
+
+    public String getBootstrapServer() {
+        return bootstrapServer;
+    }
+
+    public void setBootstrapServer(String bootstrapServer) {
+        this.bootstrapServer = bootstrapServer;
+    }
+
+    @Bean(name = KafkaStreamsDefaultConfiguration.DEFAULT_STREAMS_CONFIG_BEAN_NAME)
+    public KafkaStreamsConfiguration kafkaStreamsConfiguration() {
+        return new KafkaStreamsConfiguration(new HashMap<>());
+    }
+
 }
